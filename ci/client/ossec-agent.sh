c7d48ab277b4734a65f6b62aed5bbb5904f8d874
Roman Kutashenko <kutashenko@gmail.com>
2018-03-30 14:09:50 +0300

    Add debug info

--- ci/client/ossec-agent.sh
+++ ci/client/ossec-agent.sh
@@ -18,15 +18,31 @@ function fix_access_to_random() {
 	popd
 }
 
+echo "AGENT >>> 1"
+
 # Trap exit signals and do a proper shutdown
 trap "ossec_shutdown; exit" SIGINT SIGTERM
 
+echo "AGENT >>> 2"
+
 fix_access_to_random
+
+echo "AGENT >>> 3"
 
 # Start services
 /var/ossec/bin/agent-auth -N -d -d -d -p 1516 -m OssecServ
+
+echo "AGENT >>> 4"
+
 sleep 10
+
+echo "AGENT >>> 5" 
+
 /var/ossec/bin/ossec-control start
+
+echo "AGENT >>> 6"
 
 # Return startup events to console
 tail -f /var/ossec/logs/ossec.log
+
+echo "AGENT >>> 7"
