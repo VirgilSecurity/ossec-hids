9572676f0b2339434fe7be25b553b41741071cb6
Imelstorm <const_ant_in@virgilsecurity.com>
2018-04-02 14:56:37 +0300

    Extend start script for singe mode support

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,22 @@
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LAUNCH_SCRIPT="${SCRIPT_FOLDER}/start.scpt"
 
-osascript "${LAUNCH_SCRIPT}"
+if [ $# -le 1 ]; then
+    if [ $# -ne 0 ]; then
+        case $1 in
+            "signle")
+                osascript "${SCRIPT_FOLDER}/start_single.scpt"
+                ;;
+            "full")
+                osascript "${SCRIPT_FOLDER}/start.scpt"
+                ;;
+            *)
+                echo "illegal argument"
+                ;;
+        esac
+    else
+        osascript "${SCRIPT_FOLDER}/start.scpt"
+    fi
+else
+    echo "illegal number of parameters"
+fi
